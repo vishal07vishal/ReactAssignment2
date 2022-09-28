140ac580952d99fb7b5f1115b75f3ed5c7cc6bed
vishal <104211252+vishal07vishal@users.noreply.github.com>
2022-09-28 16:50:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Form} from "./component/Form"
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>EMOLOYEE FEEDBACK FORM</h1>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
